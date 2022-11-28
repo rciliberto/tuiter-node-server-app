ec4e12798b852cab45500ce4adc1f63a03e43446
Robby Ciliberto <robby@ciliber.to>
2022-11-28 13:20:58 -0500

    A9 - Add mongo server

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import mongoose from "mongoose";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
   || 'mongodb://localhost:27017/tuiter'
 console.log(CONNECTION_STRING)
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors())
 app.use(express.json())
